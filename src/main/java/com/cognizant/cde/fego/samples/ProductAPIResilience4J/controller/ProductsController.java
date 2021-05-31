2245f4ee176831bd172c64e51fef739e74d45f38
root <root@upshift-javatransformer-deployment-5b5dff94f9-rww6z>
2021-05-31 04:41:38 +0000

    Implementing Caching using Spring Cache and Spring Data Redis libraries

--- src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/controller/ProductsController.java
+++ src/main/java/com/cognizant/cde/fego/samples/ProductAPIResilience4J/controller/ProductsController.java
@@ -4,8 +4,10 @@ import com.cognizant.cde.fego.samples.ProductAPIResilience4J.model.Product;
 import com.cognizant.cde.fego.samples.ProductAPIResilience4J.service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.CacheEvict;
 
 @RestController
 @RequestMapping("/products")
@@ -14,24 +16,27 @@ public class ProductsController {
     @Autowired
     private ProductsService productsService;
 
+    // @Cacheable(value = “<cache-name>”, key = “<#id>”)
     @GetMapping
     public List<Product> getProducts() {
         return this.productsService.getProducts();
     }
 
+    // @CachePut(value = “<cache-name”>, key = “<“#id>)
     @PostMapping
     public void createProduct(@RequestBody Product product) {
         this.productsService.createProduct(product);
     }
 
+    // @CachePut(value = “<cache-name”>, key = “<“#id>)
     @PutMapping("/{id}")
     public void updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
         this.productsService.updateProduct(product);
     }
 
+    // @CacheEvict (value = “<cache-name”>, key = “<“#id>)
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable("id") long id) {
         this.productsService.deleteProduct(id);
     }
-
 }
